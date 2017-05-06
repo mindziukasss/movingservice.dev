6d1ccd6b8e65ca04452eaaf6ede9700aa74fe33d
Mindaugas Bernotas <mindaugasbernotas2@gmail.com>
2017-05-06 11:43:41 +0300

    added base model

---
+++ app/Models/MSBaseModel.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Ramsey\Uuid\Uuid;
+
+class MSBaseModel extends Model
+{
+    use SoftDeletes;
+
+    public $incrementing = false;
+
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+
+            $model->{$model->getKeyName()} = (string)$model->generateNewId();
+        });
+    }
+
+    /**
+     *
+     * @return \Ramsey\Uuid\UuidInterface
+     */
+    public function generateNewId()
+    {
+        if (isset($this->attributes['id'])) {
+            return $this->attributes['id'];
+        }
+
+        return Uuid::uuid4();
+
+    }
+}
+
